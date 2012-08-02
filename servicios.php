6590dab786ac34fcaeb19fe5b4a70f7c26a5516d
root <root@maximiliano-N100.(none)>
2012-08-01 22:52:34 -0400

    M-01-08-2012-22:52

---
+++ servicios.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Titulo de la web</title>
+        <script type="text/javascript" src="js/modernizr.custom.79584.js"></script>
+        <link  type="text/css" rel="stylesheet" href="reset.css"/>
+        <link  type="text/css" rel="stylesheet" href="servicios.css"/>
+        <link  type="text/css" rel="stylesheet" media="screen and (max-device-width: 5000px)" href="res1025.css"/>
+        <link  type="text/css" rel="stylesheet" media="screen and (max-device-width: 1024px)" href="res1024-servicios.css"/>
+        <link  type="text/css" rel="stylesheet" media="screen and (max-device-width: 800px)" href="res600.css"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    </head>
+    <body>
+        <div id="contenido">
+            <header>
+                <div class="titulo-logo">
+                    <h1>Entweb</h1>
+                </div>            
+            </header>
+            <nav>
+                <ul>
+                    <li><a href="">enlace 1</a></li>
+                    <li><a href="">enlace 2</a></li>
+                    <li><a href="">enlace 3</a></li>
+                </ul>
+            </nav>
+            <section>
+                <article>
+                    <h2>¿Porque tener una web?</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In blandit luctus leo vitae volutpat. Sed imperdiet scelerisque consequat. Nullam eu turpis elit. Donec libero massa, interdum et lobortis sed, consequat at ante. Mauris tortor sapien, fermentum vitae dictum et, commodo at sem. Praesent neque lectus, bibendum quis consequat nec, porttitor eu ante. Sed risus nisi, tincidunt sodales mattis vitae, vulputate eget tellus. Integer id urna mi, sit amet imperdiet ipsum. Mauris rhoncus turpis nec sem facilisis et pellentesque diam tincidunt. </p>
+                </article>
+            </section>
+            <footer>
+                <h6> (c)Copyright 2012</h6>
+            </footer>
+        </div>
+    </body>
+</html>
