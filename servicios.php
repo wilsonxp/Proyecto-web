8e1d2dc9c59a65dcbcd0fbd31895b9cc0dff1473
mopitz <mopitz@mopitz-Dell-System-Vostro-3750.(none)>
2012-08-06 16:08:17 -0400

    Maxi

--- servicios.php
+++ servicios.php
@@ -174,7 +174,7 @@
                                 <td>Si ningun plan es adecuado para tus nesesidades, puedes elegir este plan y contruirlo a tu medida</td>
                             </tr>
                         </table>
-                        <div style="border:1px solid black;">
+                        <div>
                             <svg>
                                 <polygon points="0,0 36,0 18,30" fill="#CFCFCF">
                             </svg>
