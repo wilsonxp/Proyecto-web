41d9e07f53c72dbe1bd853ffb9aa74658c884b7e
Maximiliano <mopitz199@gmail.com>
2012-08-07 21:00:26 -0400

    M-07-08-2012-21:00

--- index.php
+++ index.php
@@ -22,6 +22,9 @@
                     <li><a href="https://plus.google.com"><img class="iconos" src="imagenes/google+.png"/></a></li>
                 </ul>            
             </header>
+            <div id="bienvenida">
+                <p><span>Bienvenido</span> Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
             <nav>
                 <ul>
                     <li class="li1">
