458a272d4fecd4d1c2fb5b79680fb98a15c02b44
root <root@erick-Inspiron-N7010.(none)>
2012-08-10 13:58:07 -0400

    Este si esta bueno

---
+++ servicios.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Titulo de la web</title>
+        <script type="text/javascript" src="js/modernizr.custom.79584.js"></script>
+        <link  type="text/css" rel="stylesheet" href="css/general/reset.css"/>
+        <link  type="text/css" rel="stylesheet" href="css/servicios/servicios.css"/>
+        <link  type="text/css" rel="stylesheet" media="screen and (max-device-width: 5000px)" href="css/servicios/res1025-servicios.css"/>
+        <link  type="text/css" rel="stylesheet" media="screen and (max-device-width: 1024px)" href="css/servicios/res1024-servicios.css"/>
+        <link  type="text/css" rel="stylesheet" media="screen and (max-device-width: 800px)" href="css/servicios/res600.css"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    </head>
+    <body>
+        <div id="contenido">
+            <header>
+                <div class="titulo-logo">
+                    <h1>VidaWeb</h1>
+                </div>
+                <ul>
+                    <li><a href="https://www.facebook.com/"><img class="iconos" src="imagenes/facebook.png"/></a></li>
+                    <li><a href="http://twitter.com/"><img class="iconos" src="imagenes/twitter.png"/></a></li>
+                    <li><a href="https://plus.google.com"><img class="iconos" src="imagenes/google+.png"/></a></li>
+                </ul>            
+            </header>
+            <nav>
+                <ul>
+                    <li class="li1">
+                        <a href="index.php">Inicio</a>
+                    </li>
+                    <li class="li2">
+                        <a href="">¿Quienes somos?</a>
+                    </li>
+                    <li class="li3">
+                        <a href="">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+            <section>
+                <article>
+                    <div class="pack1">
+                        <table>
+                            <tr>
+                                <th>Plan basico</th>
+                            </tr>
+                            <tr>
+                                <td>5 paginas</td>
+                            </tr>
+                            <tr>
+                                <td>Logo incluido</td>
+                            </tr>
+                            <tr>
+                                <td>Formulario contacto</td>
+                            </tr>
+                            <tr>
+                                <td>Uso HTML5</td>
+                            </tr>
+                            <tr>
+                                <td>Uso CSS3</td>
+                            </tr>
+                            <tr>
+                                <td>Estatica</td>
+                            </tr>
+                            <tr>
+                                <td>Base de datos</td>
+                            </tr>
+                        </table>
+                        <div class="caja-precio">
+                            <p class="desde">desde</p>
+                            <p class="precio">$80.000</p><span class="no-incluye-host">(no incluye host)</span>
+                        </div>
+                        <div>
+                            <svg>
+                                <polygon points="0,0 36,0 18,30" fill="#CFCFCF" style="stroke:white; stroke-width:1;">
+                            </svg>
+                        </div>
+                    </div>                  
+                    <div class="pack2">
+                        <table>
+                            <tr>
+                                <th>Plan medio</th>
+                            </tr>
+                            <tr>
+                                <td>10 paginas</td>
+                            </tr>
+                            <tr>
+                                <td>Logo incluido</td>
+                            </tr>
+                            <tr>
+                                <td>Formulario contacto</td>
+                            </tr>
+                            <tr>
+                                <td>Uso HTML5</td>
+                            </tr>
+                            <tr>
+                                <td>Uso CSS3</td>
+                            </tr>
+                            <tr>
+                                <td>Estatica</td>
+                            </tr>
+                            <tr>
+                                <td>Base de datos</td>
+                            </tr>
+                        </table>
+                        <div class="caja-precio">
+                            <p class="desde">desde</p>
+                            <p class="precio">$80.000</p><span class="no-incluye-host">(no incluye host)</span>
+                        </div>
+                        <div>
+                            <svg>
+                                <polygon points="0,0 36,0 18,30" fill="#CFCFCF">
+                            </svg>
+                        </div>
+                    </div>                    
+                    <div class="pack3">
+                        <table>
+                            <tr>
+                                <th>Plan avanzado</th>
+                            </tr>
+                            <tr>
+                                <td>15 paginas</td>
+                            </tr>
+                            <tr>
+                                <td>Logo incluido</td>
+                            </tr>
+                            <tr>
+                                <td>Formulario contacto</td>
+                            </tr>
+                            <tr>
+                                <td>Uso HTML5</td>
+                            </tr>
+                            <tr>
+                                <td>Uso CSS3</td>
+                            </tr>
+                            <tr>
+                                <td>Estatica</td>
+                            </tr>
+                            <tr>
+                                <td>Base de datos</td>
+                            </tr>
+                        </table>
+                        <div class="caja-precio">
+                            <p class="desde">desde</p>
+                            <p class="precio">$80.000</p><span class="no-incluye-host">(no incluye host)</span>
+                        </div>
+                        <div>
+                            <svg>
+                                <polygon points="0,0 36,0 18,30" fill="#CFCFCF">
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="pack4">
+                        <table>
+                            <tr>
+                                <th>Plan aplicacion</th>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="imagenes/app-web.jpg">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>1 Base de datos</td>
+                            </tr>
+                            <tr>
+                                <td>Dinamica</td>
+                            </tr>
+                            <tr>
+                                <td>Mysql</td>
+                            </tr>
+                        </table>
+                        <div class="caja-precio">
+                            <p class="desde">desde</p>
+                            <p class="precio">$80.000</p><span class="no-incluye-host">(no incluye host)</span>
+                        </div>
+                        <div>
+                            <svg>
+                                <polygon points="0,0 36,0 18,30" fill="#CFCFCF">
+                            </svg>
+                        </div>
+                    </div>                   
+                    <div class="pack5">
+                        <table>
+                            <tr>
+                                <th>Plan dinamico</th>
+                            </tr>
+                                <td>
+                                    <img src="imagenes/dinamico.jpg">
+                                </td>
+                            <tr>
+                                <td>Si ningun plan es adecuado para tus nesesidades, puedes elegir este plan y contruirlo a tu medida</td>
+                            </tr>
+                        </table>
+                        <div style="/*border:1px solid black;*/">
+                            <div class="caja-precio">
+                                <p class="desde">desde</p>
+                                <p class="precio">$80.000</p><span class="no-incluye-host">(no incluye host)</span>
+                            </div>
+                        <div>
+                            <svg>
+                                <polygon points="0,0 36,0 18,30" fill="#CFCFCF">
+                            </svg>
+                        </div>
+                    </div>   
+                    <div style="clear:both;"></div>        
+                </article>
+            </section>
+            <footer>
+                <h6> (c)Copyright 2012</h6>
+            </footer>
+        </div>
+    </body>
+</html>
