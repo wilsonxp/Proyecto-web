299ddeab5ed36fb195e76b83154dcb0dfce368b0
Maximiliano <mopitz199@gmail.com>
2012-08-15 21:57:08 -0400

    mopitz

--- servicios.php
+++ servicios.php
@@ -38,7 +38,7 @@
             <div id="bienvenida">
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
-            <section>
+            <section id="tablas">
                 <article>
                     <div class="pack1">
                         <table>
@@ -197,7 +197,7 @@
                     <div style="clear:both;"></div>        
                 </article>
             </section>
-            <section>
+            <section id="des-tablas">
                 <h2>Descripcion</h2>
                 <article>
                     <h3>Plan basico</h3>
