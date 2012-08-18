528a9fe94ce10dd55b9a1b259e72ec428587ddd1
root <root@erick-Inspiron-N7010.(none)>
2012-08-17 23:43:24 -0400

    flechas

---
+++ imagenes/flecha.php
@@ -0,0 +1,145 @@
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="65.845779"
+   height="61.331814"
+   id="svg2"
+   version="1.1"
+   inkscape:version="0.48.3.1 r9886"
+   sodipodi:docname="Nuevo documento 1">
+  <defs
+     id="defs4">
+    <linearGradient
+       inkscape:collect="always"
+       id="linearGradient5276">
+      <stop
+         style="stop-color:#cfcfcf;stop-opacity:1;"
+         offset="0"
+         id="stop5278" />
+      <stop
+         style="stop-color:#cfcfcf;stop-opacity:0;"
+         offset="1"
+         id="stop5280" />
+    </linearGradient>
+    <linearGradient
+       id="linearGradient3786">
+      <stop
+         style="stop-color:#cfcfcf;stop-opacity:1;"
+         offset="0"
+         id="stop3788" />
+      <stop
+         style="stop-color:#000000;stop-opacity:0;"
+         offset="1"
+         id="stop3790" />
+    </linearGradient>
+    <linearGradient
+       inkscape:collect="always"
+       xlink:href="#linearGradient3786"
+       id="linearGradient5246"
+       gradientUnits="userSpaceOnUse"
+       x1="69.992683"
+       y1="527.40289"
+       x2="69.992683"
+       y2="482.26233"
+       gradientTransform="translate(36.943453,-41.40635)" />
+    <linearGradient
+       inkscape:collect="always"
+       xlink:href="#linearGradient5276"
+       id="linearGradient5282"
+       x1="76.041344"
+       y1="487.68182"
+       x2="76.041344"
+       y2="459.88882"
+       gradientUnits="userSpaceOnUse" />
+    <filter
+       inkscape:collect="always"
+       id="filter5330"
+       color-interpolation-filters="sRGB">
+      <feGaussianBlur
+         inkscape:collect="always"
+         stdDeviation="0.43415062"
+         id="feGaussianBlur5332" />
+    </filter>
+    <linearGradient
+       inkscape:collect="always"
+       xlink:href="#linearGradient5276"
+       id="linearGradient5338"
+       gradientUnits="userSpaceOnUse"
+       x1="76.041344"
+       y1="487.68182"
+       x2="76.041344"
+       y2="459.88882" />
+  </defs>
+  <sodipodi:namedview
+     id="base"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageopacity="0.0"
+     inkscape:pageshadow="2"
+     inkscape:zoom="1"
+     inkscape:cx="-8.3698932"
+     inkscape:cy="68.198429"
+     inkscape:document-units="px"
+     inkscape:current-layer="layer1"
+     showgrid="false"
+     fit-margin-top="0"
+     fit-margin-left="0"
+     fit-margin-right="0"
+     fit-margin-bottom="0"
+     inkscape:window-width="772"
+     inkscape:window-height="383"
+     inkscape:window-x="85"
+     inkscape:window-y="197"
+     inkscape:window-maximized="0">
+    <inkscape:grid
+       type="xygrid"
+       id="grid2985"
+       empspacing="5"
+       visible="true"
+       enabled="true"
+       snapvisiblegridlinesonly="true"
+       originx="-59.971573px"
+       originy="-532.07622px" />
+  </sodipodi:namedview>
+  <metadata
+     id="metadata7">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title></dc:title>
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     inkscape:label="Capa 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(-59.971573,-458.95416)">
+    <g
+       id="g5334"
+       transform="matrix(2.034464,0,0,2.034464,-62.010898,-474.46156)">
+      <path
+         style="fill:none;stroke:#cfcfcf;stroke-width:0.30000001;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:none;filter:url(#filter5330)"
+         d="m 71.136919,474.84647 0,-15 m 9.999999,0 0,15 m 10,-5 0,10 c -5.12671,2.4041 -10.1014,5.05855 -15,7.88675 l -14.999999,-7.88675 0,-10"
+         id="path5284"
+         inkscape:connector-curvature="0"
+         sodipodi:nodetypes="ccccccccc" />
+      <path
+         sodipodi:nodetypes="cccccccccc"
+         inkscape:connector-curvature="0"
+         id="path3784"
+         d="m 71.034098,459.70353 10,0 0,15 10,-5 0,10 c -5.126714,2.4041 -10.101406,5.05855 -15,7.88675 l -15,-7.88675 0,-10 10,5 z"
+         style="fill:url(#linearGradient5338);fill-opacity:1;stroke:none" />
+    </g>
+  </g>
+</svg>
