fdabada3b5d2d65d4c989f87956f589bacee0d84
root <root@maximiliano-N100.(none)>
2012-08-20 11:38:14 -0400

    mopitz

---
+++ contacto.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Contacto</title>
+	<link  type="text/css" rel="stylesheet" href="css/general/reset.css"/>
+	<link rel="stylesheet" type="text/css" href="css/contacto.css">
+</head>
+<body>
+	<div id="contenido">
+		<header>
+            <div class="titulo-logo">
+                <a href="index.php"><?php include("imagenes/logo.php"); ?></a>
+            </div>
+            <ul>
+                <li><a href="https://www.facebook.com/"><img class="iconos" src="imagenes/facebook.png"/></a></li>
+                <li><a href="http://twitter.com/"><img class="iconos" src="imagenes/twitter.png"/></a></li>
+                <li><a href="https://plus.google.com"><img class="iconos" src="imagenes/google+.png"/></a></li>
+            </ul>            
+        </header>
+        <nav>
+            <ul>
+                <li class="li1">
+                    <a href="index.php">Inicio</a>
+                </li>
+                <li class="li2">
+                    <a href="">¿Quienes somos?</a>
+                </li>
+                <li class="li3">
+                    <a href="">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+        <section id="sec-formulario">
+        	<form action="#" method="post" enctype="form/multipart">
+        		<div class="entradas">
+        			<p>Nombre</p><input type="name" autofocus>
+        		</div>
+        		<div class="entradas">
+        			<p>Email</p><input type="email">
+        		</div>
+        		<div class="entradas">
+        			<p>Asunto</p><input type="text">
+        		</div>
+        		<div class="entradas">
+        			<p>Consulta</p><textarea></textarea>
+        		</div>
+        		<div>
+        			<input type="submit" value="Enviar" name="enviar">
+        		</div>
+        	</form>
+        </section>
+	</div>
+</body>
+</html>
